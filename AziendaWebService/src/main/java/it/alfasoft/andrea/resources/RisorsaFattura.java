25f22d77368b9a46c93e6f01efb1f925f0c347aa
and86 <andrea.ln@tiscali.it>
2016-11-20 17:46:58 +0100

    report da completare

--- AziendaWebService/src/main/java/it/alfasoft/andrea/resources/RisorsaFattura.java
+++ AziendaWebService/src/main/java/it/alfasoft/andrea/resources/RisorsaFattura.java
@@ -3,11 +3,13 @@ package it.alfasoft.andrea.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.alfasoft.andrea.bean.FiltriFattura;
 import it.alfasoft.andrea.model.Fattura;
 import it.alfasoft.andrea.service.Servizio;
 
 
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -17,8 +19,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
-import oracle.jdbc.proxy.annotation.Post;
 
 
 @Path("fattura")
@@ -45,6 +45,15 @@ public class RisorsaFattura {
 		s.registraFattura(f);
 		return Response.status(Status.CREATED).entity(f).build();
 		
+	}
+	
+	@GET
+	@Path("/fatturaProva")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getContextParameter(@BeanParam FiltriFattura ff){
+		
+//		ff.setAnno(2016);
+		return ff.getDateInizio()+ " "+ff.getDateFine();
 	}
 
 	
--- AziendaWebService/src/main/java/it/alfasoft/andrea/service/Servizio.java
+++ AziendaWebService/src/main/java/it/alfasoft/andrea/service/Servizio.java
@@ -9,11 +9,13 @@ public class Servizio {
 	
 	FatturaDao fDao=new FatturaDao();
 	
+	//Registrazione
 	public boolean registraFattura(Fattura f){
 		return fDao.creaFattura(f);	
 		
 	}
 	
+	//Lettura
 	public Fattura leggiFatturaConCodice(String codice){
 		Fattura f=fDao.leggiFatturaConCodice(codice);
 		return f;
@@ -22,12 +24,15 @@ public class Servizio {
 	public List<Fattura> leggiTutteFatture(){
 		return fDao.leggiTutteFatture();
 	}
+
 	
+	//Update
 	public boolean aggiornaFattura(Fattura f) {
 		fDao.leggiFatturaConId(f.getId_fattura());
 		return fDao.aggiornaFattura(f);
 	}
 	
+	//Delete
 	public boolean eliminaFatturaConCodice(String codice){
 		Fattura f= fDao.leggiFatturaConCodice(codice);
 		return fDao.eliminaFattura(f);
